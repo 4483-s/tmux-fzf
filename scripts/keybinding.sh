e5a984d44669224e41cdd37a91cf2313ad89a7ad
wei <188955756+4483-s@users.noreply.github.com>
2026-09-07 01:40:01 +0800

    fix: tmux commands through "keybinding" may fail because of curly braces

--- scripts/keybinding.sh
+++ scripts/keybinding.sh
@@ -7,7 +7,5 @@ source "$CURRENT_DIR/.envs"
 target=$(tmux list-keys | sed '1s/^/[cancel]\n/' | eval "$TMUX_FZF_BIN $TMUX_FZF_OPTIONS")
 
 [[ "$target" == "[cancel]" || -z "$target" ]] && exit
-if [[ -n $(echo "$target" | grep -o "copy-mode") && -z $(echo "$target" | grep -o "prefix") ]]; then
-    tmux copy-mode
-fi
-echo "$target" | awk '{ $1=$2=$3=$4=""; print $0 }' | sed 's/^ *//' | xargs tmux
+[[ $target = *copy-mode* && ! "$target" = *prefix* ]] && tmux copy-mode
+tmux source /dev/stdin < <(echo "$target" | awk '{ $1=$2=$3=$4=""; print $0 }')
