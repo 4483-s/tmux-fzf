646efda2c648e941b59ed651384a603c2193e136
wei <188955756+4483-s@users.noreply.github.com>
2026-09-09 12:51:05 +0800

    untangle obsecure sh string, ensure content is pasted as a single unit use normal array instead of associative array

--- scripts/clipboard.sh
+++ scripts/clipboard.sh
@@ -14,18 +14,18 @@ fi
 
 if [[ "$action" == "system" ]]; then
     item_numbers=$(copyq count)
-    index=0
-    declare -A obj
-    while [ "$index" -lt "$item_numbers" ]; do
-        item_value=$(copyq read ${index})
-        # real newlines are converted to spaces, each key is a line in fzf
-        item_key="copy${index}: ${item_value//$'\n'/ }"
-        obj[$item_key]=$item_value
-        index=$((index + 1))
+    arr=()
+    for ((i = 0; i < item_numbers; i++)); do
+      item_value=$(copyq read ${i})
+      item_key="copy${i}: ${item_value//$'\n'/ }"
+      arr+=("$item_key")
     done
-    copyq_index=$(printf '%s\n' "${!obj[@]}"|sort -V| eval "$TMUX_FZF_BIN $TMUX_FZF_OPTIONS --preview=\"echo {} | sed -e 's/^copy//' -e 's/: .*//' | xargs -I, copyq read , 2> /dev/null\"" | sed -e 's/^copy//' -e 's/: .*//')
+    copyq_index=$(printf '%s\n' "${arr[@]}"| eval "$TMUX_FZF_BIN $TMUX_FZF_OPTIONS --preview=\"echo {} | sed -e 's/^copy//' -e 's/: .*//' | xargs -I, copyq read , 2> /dev/null\"" | sed -e 's/^copy//' -e 's/: .*//')
     [[ -z "$copyq_index" ]] && exit
-    echo "$copyq_index" | xargs -I{} sh -c 'tmux set-buffer -b _temp_tmux_fzf "$(copyq read {})" && tmux paste-buffer -b _temp_tmux_fzf && tmux delete-buffer -b _temp_tmux_fzf'
+    while read -r i;do
+      paste_content+=$(copyq read "$i")
+    done <<< "$copyq_index"
+    tmux set-buffer -b _temp_tmux_fzf "${paste_content}" && tmux paste-buffer -b _temp_tmux_fzf && tmux delete-buffer -b _temp_tmux_fzf
 elif [[ "$action" == "buffer" ]]; then
     selected_buffer=$(tmux list-buffers | sed -e 's/:.*bytes//' -e '1s/^/[cancel]\n/' -e 's/: "/: /' -e 's/"$//' | eval "$TMUX_FZF_BIN $TMUX_FZF_OPTIONS --preview=\"echo {} | sed -e 's/\[cancel\]//' -e 's/:.*$//' | head -1 | xargs tmux show-buffer -b\"" | sed 's/:.*$//')
     [[ -z "$selected_buffer" ]] && exit
